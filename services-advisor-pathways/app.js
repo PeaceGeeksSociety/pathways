92b6d0114c85406bc56eccb28ffc0e39bf04eb44
Tucker Buchy <tucker.buchy@gmail.com>
2017-07-24 12:39:56 -0700

    added pathwaays end point

--- services-advisor-pathways/app.js
+++ services-advisor-pathways/app.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var services = require('./routes/services');
+var pathways = require('./routes/pathways');
 
 var app = express();
 mongoose.connect('mongodb://localhost:28000/bc211');
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/services', services);
+app.use('/pathways', pathways);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
